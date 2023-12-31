8b614b462b506d597d5f22afb74d56dca0691be1
mohesham88 <67235854+mohesham88@users.noreply.github.com>
2023-12-31 15:53:08 +0200

    clean shipment error response

--- src/main/java/com/example/softwareassignment2/Services/ShipmentService.java
+++ src/main/java/com/example/softwareassignment2/Services/ShipmentService.java
@@ -1,6 +1,7 @@
 package com.example.softwareassignment2.Services;
 
 
+import com.example.softwareassignment2.DTO.ShipmentResponse;
 import com.example.softwareassignment2.Models.*;
 import com.example.softwareassignment2.Repositories.CustomerRepository;
 import com.example.softwareassignment2.Repositories.OrderRepository;
@@ -21,24 +22,29 @@ public class ShipmentService {
     private CustomerRepository customerRepository;
 
 
-    public Shipment createShipment(int orderId) {
-
+    public ShipmentResponse createShipment(int orderId) {
+        ShipmentResponse shipmentResponse = new ShipmentResponse();
+        shipmentResponse.setShipmentDetails(null);
         Order order = orderRepository.getOrderById(orderId);
 
         Shipment shipment = new Shipment();
 
 
         // if the order exist in the database make the shipment
-        if(order != null && reduceShippingFeesFromCustomers(order, shipment.getShipmentFees())){
+        if(order != null){
+            if(!reduceShippingFeesFromCustomers(order, shipment.getShipmentFees())){
+                shipmentResponse.setError("customer doesn't have enough balance to pay the shipping fees");
+                return  shipmentResponse;
+            }
             shipment.setOrder(order);
             shipment.setStatus(ShipmentStatus.PENDING);
             shipmentRepository.addShipment(shipment);
-            return shipment;
+            shipmentResponse.setShipmentDetails(shipment);
         }
-        // else return null
         else{
-            return null;
+            shipmentResponse.setError("order id is not valid, order doesn't exist");
         }
+        return shipmentResponse;
     }
 
 
@@ -47,31 +53,36 @@ public class ShipmentService {
         if(order instanceof SimpleOrder){
             System.out.println(((SimpleOrder) order).getCustomerID());
             int customerId = ((SimpleOrder) order).getCustomerID();
-            if(!reduceShippingFeesFromCustomer(customerId, shipmentFees))
+            if(checkIfUserHaveEnoughBalance(customerId, shipmentFees)){
+                reduceShippingFeesFromCustomer(customerId, shipmentFees);
+            }else {
                 return false;
+            }
         }else{
             // compound order
             // divide the fees among the customers
             List<SimpleOrder> simpleOrders = ((CompoundOrder)order).getSimpleOrders();
             double shipmentFeesDivided = shipmentFees / simpleOrders.size();
             for(SimpleOrder simpleOrder : simpleOrders){
-                if(!reduceShippingFeesFromCustomer(simpleOrder.getCustomerID(), shipmentFeesDivided)){
+                if(checkIfUserHaveEnoughBalance(simpleOrder.getCustomerID(), shipmentFeesDivided)){
                     return false;
                 }
             }
+            // if they all have enough balance reduce the shipping fees
+            for(SimpleOrder simpleOrder : simpleOrders){
+                reduceShippingFeesFromCustomer(simpleOrder.getCustomerID(), shipmentFeesDivided);
+            }
         }
-
         return true;
     }
 
-    public boolean reduceShippingFeesFromCustomer(int customerId, double shipmentFees){
+    public void reduceShippingFeesFromCustomer(int customerId, double shipmentFees){
         Customer customer = customerRepository.getCustomerByID(customerId);
+        customer.getCustomerAccount().setAccountBalance(customer.getCustomerAccount().getAccountBalance() - shipmentFees);
+    }
 
-        if(customer.getCustomerAccount().getAccountBalance() >= shipmentFees){
-            customer.getCustomerAccount().setAccountBalance(customer.getCustomerAccount().getAccountBalance() - shipmentFees);
-            return true;
-        }else {
-            return false;
-        }
+    private boolean checkIfUserHaveEnoughBalance(int customerId ,double shippingFees){
+        Customer customer = customerRepository.getCustomerByID(customerId);
+        return customer.getCustomerAccount().getAccountBalance() >= shippingFees;
     }
 }
